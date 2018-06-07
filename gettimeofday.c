6cbd61e8646be16bdb2c60d9d5306a88e408c6a1
synical <sjohnsondot@gmail.com>
2018-06-07 21:50:37 +0000

    Run test for all clocksources in system

--- gettimeofday.c
+++ gettimeofday.c
@@ -1,8 +1,9 @@
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <fcntl.h>
 #include <string.h>
+#include <unistd.h>
+#include <time.h>
 
 /*
  TODO
@@ -11,38 +12,56 @@
 
 #define CURRENT_CLOCK_PATH "/sys/devices/system/clocksource/clocksource0/current_clocksource"
 
+static int loop_count = 5000000;
+
 void loop_gettimeofday(int loop_count, struct timeval *tv) {
     int i = 0;
-    for(;i<loop_count;i++) {
-        gettimeofday(tv,0);
+    for(i=0 ; i<loop_count; i++) {
+        gettimeofday(tv, 0);
     }
 }
 
 void set_clocksource(char *clocksource) {
-    int fd, r;
+    int fd, res;
+
     fd = open(CURRENT_CLOCK_PATH, O_RDWR);
-    r = write(fd, clocksource, sizeof(clocksource));
-    if(r == -1) {
-        perror("open()");
+    res = write(fd, clocksource, strlen(clocksource));
+    if(res == -1) {
+        perror("write()");
         exit(EXIT_FAILURE);
     }
 }
 
-int main(int argc, char *argv[]) {
-    int i;
-    int loop_count = 5000000;
+void benchmark_available_clocksources(void) {
+    int fd, res;
+    char *buf, *token;
     struct timeval tv;
 
-    if (argc < 2) {
-        printf("Usage: %s <clocksource>\n", argv[0]);
+    buf = (char *)malloc(4096);
+    memset(buf, '\0', 4096);
+
+    fd = open("/sys/devices/system/clocksource/clocksource0/available_clocksource", O_RDONLY);
+    if (fd == -1 ) {
+        perror("open()");
         exit(EXIT_FAILURE);
     }
 
-    char *clocksource = argv[1];
+    res = read(fd, buf, 4096);
+    if (res == -1) {
+        perror("read()");
+        exit(EXIT_FAILURE);
+    }
 
-    printf("Setting clocksource to %s\n", argv[1]);
-    set_clocksource(clocksource);
-    printf("Calling gettimeofday %d times\n", loop_count);
-    loop_gettimeofday(5000000, &tv);
-    printf("Cpu time taken: %.3fs\n", ((double)clock())/CLOCKS_PER_SEC);
+    while ((token = strsep(&buf, " "))) {
+        if (strlen(token) == 1) break;
+        printf("Setting clocksource to %s\n", token);
+        set_clocksource(token);
+        printf("Calling gettimeofday %d times\n", loop_count);
+        loop_gettimeofday(loop_count, &tv);
+        printf("Cpu time taken: %.3fs\n", ((double)clock())/CLOCKS_PER_SEC);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    benchmark_available_clocksources();
 }
